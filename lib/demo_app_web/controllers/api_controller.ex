6f71e9dff1d6cc743ceae7127e18e5805b29b66d
katestud <kate@automatedinsights.com>
2018-05-07 17:15:07 -0400

    Read request body instead of params

--- lib/demo_app_web/controllers/api_controller.ex
+++ lib/demo_app_web/controllers/api_controller.ex
@@ -1,11 +1,12 @@
 defmodule DemoAppWeb.ApiController do
   use DemoAppWeb, :controller
 
-  def sns_webhook(conn, _params) do
-    IO.inspect("================")
-    IO.inspect("RECEIVING SNS MESSAGE")
-    IO.inspect("================")
-    IO.inspect(conn)
+  def sns_webhook(conn, params) do
+    {:ok, message_data, _conn_details} = Plug.Conn.read_body(conn)
+    IO.puts("================")
+    IO.puts("RECEIVING SNS MESSAGE")
+    IO.inspect(message_data)
+    IO.puts("================")
     json conn, %{success: true}
   end
 end
